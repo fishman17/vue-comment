b7b25c0d63318a61860f16a0baaa856dbef7999d
fishman17 <734583898@qq.com>
2018-01-10 14:27:13 +0800

    'vue-comment'

---
+++ src/components/CommentItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      {{comment.text}}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['comment']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <el-col :span="24" class="pagination">
+        <el-pagination layout="prev, pager, next" :current-page="currentPage"
+         :page-size="pagesize" :total="totalCount" style="float:right;"
+         @current-change="handleCurrentPage">
+        </el-pagination>
+    </el-col>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['totalCount', 'currentPage'],
+    data() {
+        return {
+            pagesize: 3
+        }
+    },
+    methods: {
+        handleCurrentPage(val) {
+            this.$emit('transferPage', val);
+        }
+    }
+}
+</script>
+
+<style>
+.toolbar {
+  background: #f2f2f2;
+  padding: 10px;
+  margin: 10px 0px;
+}
+.el-form-item {
+  margin-bottom: 10px;
+}
+
+</style>
